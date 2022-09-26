141c3bd9e7c179323c7b5f1e3f7795f4391c9a28
Kehinde Onifade <onifkay@gmail.com>
2022-09-26 17:19:36 +0100

    Fix linter errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
-import React from "react";
-import Navbar from "./Navbar";
-import logo from "../assets/images/planet.png";
+import React from 'react';
+import Navbar from './Navbar';
+import logo from '../assets/images/planet.png';
 
 import '../css/header.css';
 
-const Header = () => {
-  return (
-    <header>
-      <div className="left-bar">
-        <img src={logo} alt="logo" />
-        <h3>Space Traveler's Hub</h3>
-      </div>
-      <Navbar />
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <div className="left-bar">
+      <img src={logo} alt="logo" />
+      <h3>Space Traveler&apos;s Hub</h3>
+    </div>
+    <Navbar />
+  </header>
+);
 
 export default Header;
