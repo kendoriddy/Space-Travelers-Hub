9e339feb3a6d10757dbb90da9dd2e35b206ac183
Kehinde Onifade <onifkay@gmail.com>
2022-09-26 23:50:42 +0100

    Populate page with data from aoi

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,32 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRocketFromApi } from '../redux/rockets/rockets';
 
 const Rockets = () => {
-  const rockets = useSelector(())
-}
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!rockets.length) dispatch(fetchRocketFromApi());
+  }, []);
+
+  return (
+    <div className="rocket-container">
+      {rockets.map((rocket) => {
+        const {
+          id, name, type, images,
+        } = rocket;
+
+        return (
+          <div key={id} className="rocket">
+            <img src={images} alt="rocket" className="rocket-image" />
+            <p>{name}</p>
+            <p>{type}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Rockets;
