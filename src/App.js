19f83e48daae0ee0a0a357806a37dab27b1594b2
Kehinde Onifade <onifkay@gmail.com>
2022-09-26 22:01:07 +0100

    Implement routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Missions from './pages/Missions';
+import Profile from './pages/Profile';
+import Rockets from './pages/Rockets';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" exact element={<Rockets />} />
+        <Route path="/missions" exact element={<Missions />} />
+        <Route path="/profile" exact element={<Profile />} />
+      </Routes>
     </div>
   );
 }
