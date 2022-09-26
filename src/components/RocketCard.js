60b371752f85dc07164d5168baa2d6e273fa4f2e
Kehinde Onifade <onifkay@gmail.com>
2022-09-26 23:16:41 +0100

    Install axios

---
+++ src/components/RocketCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import imageUse from '../assets/images/planet.png';
+
+const RocketCard = () => (
+  <div>
+    <img src={imageUse} alt="imagePic" />
+    <p>name</p>
+    <p>type</p>
+  </div>
+);
+
+export default RocketCard;
