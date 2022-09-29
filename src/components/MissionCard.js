5d016abde2e11c1e5b78c70070818941e41621a6
samiullah997 <samiullahk997@gmail.com>
2022-09-29 14:25:10 +0500

    i have use table instead of div for the list

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 import '../css/mission.css';
 
 const MissionCard = () => {
   const missions = useSelector((state) => state.mission);
   missions.forEach((element) => {
-    <div className="mission-card">
-      <div className="card">
-        <div className="card-heading">
-          <h3>Mission</h3>
-          <h3>Description</h3>
-          <h3>Status</h3>
-        </div>
-        <div className="card-body">
-          <ul>
-            <li><h2>{element.mission_name}</h2></li>
-            <li><p maxLength="700">Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></li>
-            <li><button type="button">NOT A MEMBER</button></li>
-            <li><button type="button">jOIN MISSION</button></li>
-          </ul>
-        </div>
-      </div>
-    </div>;
+    <Table striped bordered hover size="sm" className="missions-table ml-5">
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th className="desc">Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><strong>{element.mission_name}</strong></td>
+          <td>{element.description}</td>
+          <td className="td-button">
+            <h5>
+              <Badge bg="secondary">NOT A MEMBER</Badge>
+            </h5>
+          </td>
+          <td className="td-button">
+            <Button variant="outline-danger">Leave Mission</Button>
+          </td>
+        </tr>
+      </tbody>
+    </Table>;
   });
 };
 
