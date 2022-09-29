cbc40c575777a20ebb44c31d6768ed0d1dfec80c
samiullah997 <samiullahk997@gmail.com>
2022-09-29 15:13:53 +0500

    complete mission profile section

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ProfileSection from '../components/ProfileSection';
+// import MissionProfile from '../components/MissionProfile';
 
 const Profile = () => (
   <div>
     <ProfileSection />
+    {/* <MissionProfile /> */ }
   </div>
 );
 
